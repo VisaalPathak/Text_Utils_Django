00d894f33505f99800988af118cfc948fbc5b341
VisaalPathak <visaalpathak@gmail.com>
2022-02-17 18:35:14 +0545

    upper case converter added

--- Hello/views.py
+++ Hello/views.py
@@ -10,6 +10,7 @@ def analyze(request):
     djtext = request.GET.get('text', 'default')
     rmpunc = request.GET.get('removepunc', 'off')
     extraspacerm = request.GET.get('extraspacerm','off')
+    Uppercase = request.GET.get('Uppercase', 'off')
     punctuations = '''.,'":;[]{}()/|\<>!-_?*&@+'''
     analyzed_txt = ""
     if rmpunc == "on":
@@ -20,14 +21,20 @@ def analyze(request):
         return render(request, 'analyze.html', params)
     
     elif extraspacerm == "on":
-        analyzed_txt = ""
         for i, char in enumerate(djtext):
             if not(djtext[i] == " " and djtext[i+1] == " "):
                 analyzed_txt = analyzed_txt + char
-        params = {"your_analyzer":"Remove Extra Space", "analyzed_text": analyzed_txt}
+        params = {"your_analyzer":"Remove Extra Space", "analyzed_txt": analyzed_txt}
         return render(request, "analyze.html", params)
 
+    elif Uppercase == "on":
+        for char in djtext:
+            analyzed_txt += char.upper()
+        params = {"your_analyzer":"UPPER CASE CONVERTER", "analyzed_txt": analyzed_txt}
+        return render(request, "analyze.html", params)
+    
     else:
         return HttpResponse("Error")
+
 
 
